ada7cbdcdc35e6260880c98711cd21a1897838de
Tamar <tamarlolo8539@gmail.com>
2021-02-21 02:16:18 +0200

    with a change of storage method in order to keep consistency

--- app.py
+++ app.py
@@ -6,72 +6,100 @@ app_id = "efc3f977"
 api_base_url = "https://api.edamam.com/search"
 error_statement = "You should enter at least two ingredient so that we will have something to work with."
 no_results_statement = "We got no results that matches your search. We recommend either to check your spelling or to try some different ingredients."
- 
 
-app= Flask(__name__)
+
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '08642_fridge_to_table_13579'
 
-ings = []
-alls = []
-max_number_of_ingredients = 50
+
+class InputLists:
+    MAX_NUMBER_OF_INGREDIENTS = 50
+    MIN_NUMBER_OF_INGREDIENTS = 2
+
+    def __init__(self) -> None:
+        self._list = []
+
+    def _initialize_list(self) -> list:
+        self._list = []
+
+    def _add_element_to_list(self, string_to_add) -> list:
+        if string_to_add != "" and len(self._list) <= self.MAX_NUMBER_OF_INGREDIENTS:
+            self._list.append(string_to_add)
+        return self._list
+
+    def _delete_last_element(self) -> list:
+        if len(self._list) > 0:
+            del self._list[-1]
+        return self._list
+
+
+ings_object = InputLists()
+alls_object = InputLists()
 
 
 @app.route('/')
 def forms():
-    ings.clear()
-    del ings [:]
-    alls.clear()
-    del alls [:]
-    return render_template('index.html')
+    ings = ings_object._initialize_list()
+    alls = alls_object._initialize_list()
+    return render_template('index.html', ings=ings, alls=alls)
 
 
 @app.route('/data', methods=['GET', 'POST'])
 def data():
     ingredients = request.form.get("ingredients")
-    if ingredients != "":
-        ings.append(ingredients)
+    ings = ings_object._add_element_to_list(ingredients)
+
     allergies = request.form.get("allergies")
-    if allergies != "":
-        alls.append(allergies)
+    alls = alls_object._add_element_to_list(allergies)
+
     return render_template('index.html', ings=ings, alls=alls)
 
 
 @app.route('/delete/ing', methods=['GET', 'POST'])
 def delete_ings():
-    if len(ings) > 0:
-        del ings[-1]
+    ings = ings_object._delete_last_element()
+    alls = alls_object._list
+
     return render_template('index.html', ings=ings, alls=alls)
 
 
 @app.route('/delete/all', methods=['GET', 'POST'])
 def delete_alls():
-    if len(alls) > 0:
-        del alls[-1]
+    ings = ings_object._list
+    alls = alls_object._delete_last_element()
+
     return render_template('index.html', ings=ings, alls=alls)
 
 
 @app.route('/data/results', methods=['GET', 'POST'])
 def result():
-    if len(ings) < 2:
+    ings = ings_object._list
+    alls = alls_object._list
+
+    if len(ings) < ings_object.MIN_NUMBER_OF_INGREDIENTS:
         return render_template("error.html", error_statement=error_statement, alls=alls, ings=ings)
+
+    Limited = request.form.get("Limited")
+    if Limited:
+        max_number_of_ingredients = len(ings)
     else:
-        Returned = request.form.get("Returned")
-        if Returned == "1" or Returned == 1:
-            max_number_of_ingredients = len(ings)
-        else:  # Returned == "None" or Returned is None:
-            max_number_of_ingredients = 50
-        if len(alls) == 0:
-            endpoint_path = f"q={ings}&ingr={max_number_of_ingredients}"
-        else:
-            allergies_for_endpoint = '&excluded='.join(alls)
-            endpoint_path = f"q={ings}&excluded={allergies_for_endpoint}&ingr={max_number_of_ingredients}"
-        endpoint = f"{api_base_url}?app_key={app_key}&app_id={app_id}&{endpoint_path}&to=24"
-        r = requests.get(endpoint)
-        recipes = r.json()['hits']
-        if len(recipes) == 0:
-            return render_template('error.html', no_results_statement=no_results_statement)
-        else:
-            return render_template('results.html', recipes=recipes, i=endpoint)
+        max_number_of_ingredients = ings_object.MAX_NUMBER_OF_INGREDIENTS
+
+    if len(alls) == 0:
+        endpoint_path = f"q={ings}&ingr={max_number_of_ingredients}"
+    else:
+        allergies_to_exclude = '&excluded='.join(alls)
+        endpoint_path = f"q={ings}&excluded={allergies_to_exclude}&ingr={max_number_of_ingredients}"
+
+    endpoint = f"{api_base_url}?app_key={app_key}&app_id={app_id}&{endpoint_path}&to=24"
+    r = requests.get(endpoint)
+    recipes = r.json()['hits']
+    if len(recipes) == 0:
+        ings = ings_object._initialize_list()
+        alls = alls_object._initialize_list()
+        return render_template('error.html', no_results_statement=no_results_statement, alls=alls, ings=ings)
+    else:
+        return render_template('results.html', recipes=recipes, i=endpoint)
 
 
 if __name__ == '__main__':
